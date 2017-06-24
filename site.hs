8e55d19b8a54e66f1dc6eb7ff7547a39b3512ff5
Dan344 <dan-miyoshi@syake.co.jp>
2017-06-25 06:17:17 +0900

    added: deleted: 会社概要を追加+余計なファイルを削除

--- site.hs
+++ site.hs
@@ -15,7 +15,7 @@ main = hakyll $ do
         route   idRoute
         compile compressCssCompiler
 
-    match (fromList ["about.rst", "contact.markdown"]) $ do
+    match ("*.md" .||. "posts/*.md") $ do
         route   $ setExtension "html"
         compile $ pandocCompiler
             >>= loadAndApplyTemplate "templates/default.html" defaultContext
