186dea0bdce48a75bd9e50a06eb3cb965bb4d7e3
Dan344 <dan-miyoshi@syake.co.jp>
2017-06-25 06:32:11 +0900

    renamed: posts -> release

--- site.hs
+++ site.hs
@@ -15,40 +15,26 @@ main = hakyll $ do
         route   idRoute
         compile compressCssCompiler
 
-    match ("*.md" .||. "posts/*.md") $ do
+    -- ニュースリリース
+    match "release/*.md" $ do
+        route $ setExtension "html"
+        compile $ pandocCompiler
+            >>= loadAndApplyTemplate "templates/release.html"    releaseCtx
+            >>= loadAndApplyTemplate "templates/default.html" releaseCtx
+            >>= relativizeUrls
+
+    match "*.md" $ do
         route   $ setExtension "html"
         compile $ pandocCompiler
             >>= loadAndApplyTemplate "templates/default.html" defaultContext
             >>= relativizeUrls
 
-    match "posts/*" $ do
-        route $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/post.html"    postCtx
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
-            >>= relativizeUrls
-
-    create ["archive.html"] $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let archiveCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Archives"            `mappend`
-                    defaultContext
-
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-                >>= relativizeUrls
-
-
     match "index.html" $ do
         route idRoute
         compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
+            release <- recentFirst =<< loadAll "release/*"
             let indexCtx =
-                    listField "posts" postCtx (return posts) `mappend`
+                    listField "release" releaseCtx (return release) `mappend`
                     constField "title" "Home"                `mappend`
                     defaultContext
 
@@ -66,8 +52,8 @@ main = hakyll $ do
 
 
 --------------------------------------------------------------------------------
-postCtx :: Context String
-postCtx =
+releaseCtx :: Context String
+releaseCtx =
     dateField "date" "%B %e, %Y" `mappend`
     defaultContext
 
